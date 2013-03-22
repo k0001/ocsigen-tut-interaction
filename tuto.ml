a318183abc801da266d7660512353082f800b98b
Renzo Carbonara <gnuk0001@gmail.com>
2013-03-22 05:22:04 -0300

    Stuff

--- tuto.ml
+++ tuto.ml
@@ -1,26 +1,42 @@
 open Eliom_content.Html5.D
 
+
+(******************************************************************************)
+(* Services *)
+
 let main_service = Eliom_service.service ~path:[""]
   ~get_params:Eliom_parameter.unit ()
 
 let user_service = Eliom_service.service ~path:["users"]
   ~get_params:Eliom_parameter.(suffix (string "name")) ()
 
-(* User names and passwords: *)
+let connection_service = Eliom_service.post_coservice'
+    ~post_params:Eliom_parameter.(string "name" ** string "password") ()
+
+let disconnection_service = Eliom_service.void_coservice'
+
+(******************************************************************************)
+(* Users db *)
+
 let users = ref [("Calvin", "123"); ("Hobbes", "456")]
 
 let user_links () =
   let f (name,_) = li [a ~service:user_service [pcdata name] name] in
   ul (List.map f !users)
 
+let check_pwd name pwd =
+  try List.assoc name !users = pwd with Not_found -> false
+
+(******************************************************************************)
+(* Session data *)
+
 let username = Eliom_reference.eref
   ~scope:Eliom_common.default_session_scope
   ~persistent:"username" None
 
-let connection_service = Eliom_service.post_service
-    ~fallback:main_service
-    ~post_params:Eliom_parameter.(string "name" ** string "password")
-    ()
+
+(******************************************************************************)
+(* Forms *)
 
 let connection_box () =
   lwt u = Eliom_reference.get username in
@@ -39,33 +55,41 @@ let connection_box () =
                br ();
                string_input ~input_type:`Submit ~value:"Connect" ()]]) ())
 
+let disconnection_box =
+  post_form disconnection_service
+    (fun _ -> [p [string_input ~input_type:`Submit ~value:"Log out" ()]]) ()
+
+
+(******************************************************************************)
+(* Eliom registration *)
+
 
 let _ =
   Eliom_registration.Html5.register
     ~service:main_service
     (fun () () ->
       lwt cf = connection_box () in
-      Lwt.return
-        (html (head (title (pcdata "")) [])
-           (body [h1 [pcdata "Hello"];
-                  cf;
-                  user_links ()])));
+    Lwt.return
+      (html (head (title (pcdata "")) [])
+         (body [h1 [pcdata "Hello"];
+                cf;
+                user_links ()])));
 
   Eliom_registration.Html5.register
     ~service:user_service
     (fun name () ->
-      Lwt.return
-        (html (head (title (pcdata name)) [])
-           (body [h1 [pcdata name];
-                  p [a ~service:main_service [pcdata "Home"] ()]])))
+      lwt cf = connection_box () in
+    Lwt.return
+      (html (head (title (pcdata name)) [])
+         (body [h1 [pcdata name];
+                cf;
+                p [a ~service:main_service [pcdata "Home"] ()]]))) ;
 
-let check_pwd name pwd =
-  try List.assoc name !users = pwd with Not_found -> false
-
-let _ = Eliom_registration.Action.register
-  ~service:connection_service
-  (fun () (name, password) ->
+  Eliom_registration.Action.register
+    ~service:connection_service
+    (fun () (name, password) ->
       if check_pwd name password
       then Eliom_reference.set username (Some name)
-      else Lwt.return ()) ;
+      else Lwt.return ())
+
 
